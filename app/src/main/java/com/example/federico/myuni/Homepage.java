43ee333594f01a6ba2647c3b2ae8797d1b4dd2b6
Pedro18 <alessandro.pedretti1@studenti.unipr.it>
2016-03-09 11:43:17 +0100

    Pedro
    Avatar , TextView e ImageButton

--- app/src/main/java/com/example/federico/myuni/Homepage.java
+++ app/src/main/java/com/example/federico/myuni/Homepage.java
@@ -4,18 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Homepage extends AppCompatActivity {
 
+    public int prova=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        Button btnSchedule= (Button) findViewById(R.id.imageButton);
-        Button btnGraphics= (Button) findViewById(R.id.imageButton2);
-        Button btnExam= (Button) findViewById(R.id.imageButton3);
+        ImageButton btnSchedule= (ImageButton) findViewById(R.id.imageButton);
+        ImageButton btnGraphics= (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton btnExam= (ImageButton) findViewById(R.id.imageButton3);
+        TextView txtNome= (TextView) findViewById(R.id.textView);
+        TextView txtCognome=(TextView) findViewById(R.id.textView2);
+        ImageView imageMale=(ImageView) findViewById(R.id.imageView);
+        ImageView imageFemale=(ImageView) findViewById(R.id.imageView2);
 
+        if (prova==1){
+            imageFemale.setVisibility(View.VISIBLE);
+            imageMale.setVisibility(View.INVISIBLE);
+        }
+        else{
+            imageFemale.setVisibility(View.INVISIBLE);
+            imageMale.setVisibility(View.VISIBLE);
+        }
+        txtCognome.setText("Cognome");
+        txtNome.setText("Nome");
         btnSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
