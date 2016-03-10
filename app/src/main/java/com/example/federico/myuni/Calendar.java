2bbf7b946a29eda47e2e8e7a99c6e6eff91f5e88
Pedro18 <alessandro.pedretti1@studenti.unipr.it>
2016-03-10 11:24:31 +0100

    niente

--- app/src/main/java/com/example/federico/myuni/Calendar.java
+++ app/src/main/java/com/example/federico/myuni/Calendar.java
@@ -2,7 +2,9 @@ package com.example.federico.myuni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class Calendar extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class Calendar extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
 
         CalendarView calendar= (CalendarView) findViewById(R.id.calendar1);
+
 
     }
 }
