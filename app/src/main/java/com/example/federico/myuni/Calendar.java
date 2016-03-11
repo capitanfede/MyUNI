82d016524cab9b98238f07c959019382a3804793
Pedro18 <alessandro.pedretti1@studenti.unipr.it>
2016-03-11 10:54:38 +0100

    Calendario

--- app/src/main/java/com/example/federico/myuni/Calendar.java
+++ app/src/main/java/com/example/federico/myuni/Calendar.java
@@ -1,9 +1,13 @@
 package com.example.federico.myuni;
 
+import android.app.AlertDialog;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -25,7 +29,37 @@ public class Calendar extends AppCompatActivity {
             }
         });
 
+        ImageButton btnPlus =(ImageButton)findViewById(R.id.plusButton);
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // PLUS BUTTON
+
+                AlertDialog.Builder miaAlert = new AlertDialog.Builder(Calendar.this);
+                miaAlert.setTitle("inserire esame");
+                //miaAlert.setMessage("Questa è la mia prima AlertDialog");
+                //EditText txtName= (EditText) findViewById(R);
+                AlertDialog alert = miaAlert.create();
+                alert.show();
+
+            }
+        });
+
 
 
     }
+
+
+
+    public String insertEvent (String matter){ //INSERIRE LA MATERIA D'ESAME NEL CALENDARIO
+
+
+
+        return matter;
+    }
 }
+
+
+
