5f4f6ed0e1998d6a97656bb0d4edb89941e30097
capitanfede <f.raccioppi@gmail.com>
2016-03-11 10:17:16 +0100

    Fede
    Inserita pagina di registrazione

---
+++ app/src/main/java/com/example/federico/myuni/FirstRegistration.java
@@ -0,0 +1,59 @@
+package com.example.federico.myuni;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FirstRegistration extends AppCompatActivity {
+
+    EditText nome, cognome, universita, cfu;
+    TextView test;
+    String nomeS, cognomeS, universitaS;
+    Integer cfuI;
+    String MY_PREFERENCES ="DatiUser";
+
+    SharedPreferences.Editor editor;
+    SharedPreferences prefs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_registration);
+
+        nome = (EditText) findViewById(R.id.nomeET);
+        cognome = (EditText) findViewById(R.id.cognomeET);
+        universita = (EditText) findViewById(R.id.facoltaET);
+        cfu = (EditText) findViewById(R.id.cfuET);
+
+        prefs = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+        editor = prefs.edit();
+
+        Button okButton = (Button) findViewById(R.id.okButton);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nomeS = nome.getText().toString();
+                cognomeS = cognome.getText().toString();
+                universitaS = universita.getText().toString();
+                cfuI = Integer.parseInt(cfu.getText().toString());
+
+                editor.putString("nome", nomeS);
+                editor.putString("cognome", cognomeS);
+                editor.putString("univesita", universitaS);
+                editor.putInt("cfu", cfuI);
+                editor.commit();
+                
+
+            }
+        });
+
+        }
+
+    }
--- app/src/main/java/com/example/federico/myuni/MainActivity.java
+++ app/src/main/java/com/example/federico/myuni/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.federico.myuni;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    String MY_PREFERENCES ="DatiUser";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        checkFirstAccess(prefs);
+
+    }
+
+    protected void checkFirstAccess(SharedPreferences prefs){
+        if(prefs.getString("name","nulla").toString() == "nulla"){
+            Intent i = new Intent(MainActivity.this, FirstRegistration.class);
+            startActivity(i);
+            finish();
+        }
+
     }
 }
